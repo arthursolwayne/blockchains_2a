0d42572e3fcb45c44f269d1bf35e4e90839f42ea
Arthur Wayne <asw263@cornell.edu>
2024-01-29 18:38:10 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -6,9 +6,15 @@ from eth_account.messages import encode_defunct
 def sign(m):
     w3 = Web3()
     account = eth_account.Account.create()
-    eth_address = account.address
-    private_key = account.private_key
+    eth_address, private_key = create_ethereum_account()
     message_encoded = encode_defunct(text=m)
     signed_message = w3.eth.account.sign_message(message_encoded, private_key)
     assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
     return eth_address, signed_message
+
+def create_ethereum_account():
+    w3 = Web3()
+    acc = w3.eth.account.create()
+    private_key = w3.to_hex(acc.key)
+    account_address = acc.address
+    return private_key, account_address
